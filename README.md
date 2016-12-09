e5b005c2607b2639cd265f86bdd2d3769d145e1e
Jan Eberhardt <eberhardt@math.tu-berlin.de>
2016-12-09 14:45:00 +0000

    Initial Commit
    	new:     LICENSE
    	new:     README.md
    	new:     db/events.php
    	new:     lang/de/local_course_announcement.php
    	new:     lang/en/local_course_announcement.php
    	new:     lib.php
    	new:     settings.php
    	new:     version.php

---
+++ README.md
@@ -0,0 +1,17 @@
+moodle-local_course_announcement
+================================
+
+This extension allows administrators to display a message on top of every course, which can be used to reach users on an urgend matter.
+
+Usage
+=====
+
+Go to 'Site administration' > 'Plugins' > 'Local Plugins' > 'Course Announcement' and turn course announcment on (visible).
+You can define the message there as well.
+
+Styling
+=======
+
+This plugin uses the Moodle standard classes 'alert', 'alert-info', 'alert-block' and, in addition for customisation, 'courseannouncement' for displaying the message.
+You may change the CSS properties of those classes in your theme.
+In order to add the customizable class 'courseannouncement' JS is needed.
---
+++ settings.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information
+ *
+ * @package    local_course_announcement
+ * @copyright  2016 Jan Eberhardt <eberhardt@math.tu-berlin.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->locate("localplugins")) {
+    $tmp = new admin_settingpage("course_announcement", get_string("pluginname", "local_course_announcement"));
+    $tmp->add(new admin_setting_configtextarea("local_course_announcement/message",
+                                               get_string("setting_message", "local_course_announcement"),
+                                               get_string("setting_message_info", "local_course_announcement"),
+                                               ""));
+    $tmp->add(new admin_setting_configcheckbox("local_course_announcement/visible",
+                                               get_string("setting_visible", "local_course_announcement"),
+                                               get_string("setting_visible_info", "local_course_announcement"),
+                                               false));
+
+    $ADMIN->add("localplugins", $tmp);
+}
